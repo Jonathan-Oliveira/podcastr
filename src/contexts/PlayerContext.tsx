556b90e1203fa759e60805d7b552cf860c310d35
jonathan Oliveira <jonathanjorge8445@gmail.com>
2021-07-11 18:32:50 -0300

    :hammer: UPDATE: finished player functions

--- src/contexts/PlayerContext.tsx
+++ src/contexts/PlayerContext.tsx
@@ -1,4 +1,4 @@
-import { createContext } from "react";
+import { createContext, useState, ReactNode, useContext } from "react";
 
 type Episode = {
     title: string;
@@ -12,10 +12,111 @@ type PlayerContextData = {
     episodeList: Episode[];
     currentEpisodeIndex: number;
     isPlaying: boolean;
+    isLooping: boolean;
+    isShuffling: boolean;
+    hasPreviousEpisode: boolean;
+    hasNextEpisode: boolean;
     play: (episode: Episode) => void;
+    playList: (list: Episode[], index: number) => void;
+    playNext: () => void;
+    playPrevious: () => void;
     togglePlay: () => void;
-    setPlayingState: (state: boolean) => void;  
+    toggleLoop: () => void;
+    toggleShuffle: () => void;
+    clearPlayerState: () => void;
+    setPlayingState: (state: boolean) => void;
 }
 
 
 export const PlayerContext = createContext({} as PlayerContextData);
+
+type PlayerContextProviderProps = {
+    children: ReactNode;
+}
+
+export function PlayerContextProvider({ children }: PlayerContextProviderProps) {
+    const [episodeList, setEpisodeList] = useState([])
+    const [currentEpisodeIndex, setCurrentEpisodeIndex] = useState(0)
+    const [isPlaying, setIsPlaying] = useState(false)
+    const [isLooping, setIsLooping] = useState(false)
+    const [isShuffling, setIsShuffling] = useState(false)
+
+    function play(episode: Episode) {
+        setEpisodeList([episode])
+        setCurrentEpisodeIndex(0)
+        setIsPlaying(true)
+    }
+
+    function playList(list: Episode[], index: number) {
+        setEpisodeList(list)
+        setCurrentEpisodeIndex(index)
+        setIsPlaying(true)
+    }
+
+    function togglePlay() {
+        setIsPlaying(!isPlaying);
+    }
+    function toggleLoop() {
+        setIsLooping(!isLooping)
+    }
+    function toggleShuffle() {
+        setIsShuffling(!isShuffling)
+    }
+
+    function setPlayingState(state: boolean) {
+        setIsPlaying(state);
+    }
+
+    function clearPlayerState() {
+        setEpisodeList([]);
+        setCurrentEpisodeIndex(0)
+    }
+
+    const hasNextEpisode = isShuffling || (currentEpisodeIndex + 1) < episodeList.length
+    const hasPreviousEpisode = currentEpisodeIndex > 0
+
+    function playNext() {
+        if (isShuffling) {
+            const nextRandomEpisodeIndex = Math.floor(Math.random() * episodeList.length)
+            setCurrentEpisodeIndex(nextRandomEpisodeIndex);
+        } else if (hasNextEpisode) {
+            setCurrentEpisodeIndex(currentEpisodeIndex + 1)
+        }
+    }
+
+    function playPrevious() {
+        if (hasPreviousEpisode) {
+            setCurrentEpisodeIndex(currentEpisodeIndex - 1)
+        }
+    }
+
+
+    return (
+        <PlayerContext.Provider
+            value={{
+                episodeList,
+                currentEpisodeIndex,
+                play,
+                playList,
+                playNext,
+                playPrevious,
+                isPlaying,
+                isLooping,
+                isShuffling,
+                toggleShuffle,
+                toggleLoop,
+                togglePlay,
+                setPlayingState,
+                hasPreviousEpisode,
+                hasNextEpisode,
+                clearPlayerState
+            }}
+        >
+            {children}
+        </PlayerContext.Provider>
+    )
+}
+
+export const usePlayer = () => {
+    return useContext(PlayerContext)
+}
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -37,15 +37,15 @@
 // }
 
 import { GetStaticProps } from "next"
-import { useContext } from "react"
 import Image from "next/image"
+import Head from "next/head"
 import Link from "next/link"
 import { api } from "../services/api"
 import { format, parseISO } from "date-fns"
 import ptBR from "date-fns/locale/pt-BR"
 
 import { convertDurationToTimeString } from "../utils/convertDurationToTimeString"
-import { PlayerContext } from "../contexts/PlayerContext"
+import { usePlayer } from "../contexts/PlayerContext"
 
 import styles from "./home.module.scss"
 
@@ -66,16 +66,22 @@ type HomeProps = {
 
 //SSG
 export default function Home({ latestEpisodes, allEpisodes }: HomeProps) {
-  const { play } = useContext(PlayerContext)
+  const { playList } = usePlayer()
+
+  const episodeList = [...latestEpisodes, ...allEpisodes]
 
   return (
     <div className={styles.homePage}>
+
+      <Head>
+        <title> Home | Podcastr</title>
+      </Head>
 
       <section className={styles.latestEpisodes}>
         <h2>Últimos Lançamentos</h2>
 
         <ul>
-          {latestEpisodes.map(episode => {
+          {latestEpisodes.map((episode, index) => {
             return (
               <li key={episode.id}>
 
@@ -95,7 +101,7 @@ export default function Home({ latestEpisodes, allEpisodes }: HomeProps) {
                   <span>{episode.publishedAt}</span>
                   <span>{episode.durationAsString}</span>
                 </div>
-                <button type="button" onClick={() => play(episode)}>
+                <button type="button" onClick={() => playList(episodeList, index)}>
                   <img src="/play-green.svg" alt="Tocar episódio"></img>
                 </button>
               </li>
@@ -120,7 +126,7 @@ export default function Home({ latestEpisodes, allEpisodes }: HomeProps) {
 
           </thead>
           <tbody>
-            {allEpisodes.map(episode => {
+            {allEpisodes.map((episode, index) => {
               return (
                 <tr key={episode.id}>
                   <td style={{ width: 72 }}>
@@ -141,7 +147,7 @@ export default function Home({ latestEpisodes, allEpisodes }: HomeProps) {
                   <td style={{ width: 100 }}>{episode.publishedAt}</td>
                   <td>{episode.durationAsString}</td>
                   <td>
-                    <button type="button" onClick={() => play(episode)}>
+                    <button type="button" onClick={() => playList(episodeList, index + latestEpisodes.length)}>
                       <img src="/play-green.svg" alt="Tocar episódio"></img>
                     </button>
                   </td>
